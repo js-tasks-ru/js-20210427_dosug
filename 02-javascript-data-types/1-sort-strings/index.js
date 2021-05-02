7f5bae98abb185203244ba804de4cb86d8dfde09
Denis Danshin <dosug.d@gmail.com>
2021-05-02 17:50:55 +0300

    функция сортировки строк

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,27 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  if (param === 'asc') {
+    return arr.slice().sort((a, b) => {
 
+      // Проверка на идентичность строк в одном регистре для
+      // выставления вперёд строк с заглавной.
+      if (a.toUpperCase() === b.toUpperCase() &&
+          a.localeCompare(b) === 1
+        ) {
+          return -1;
+      }
+
+      return a.localeCompare(b);
+    });
+  }
+
+  if (param === 'desc') {
+    return arr.slice().sort((a, b) => {
+
+      // нет необходимости делать туже проверку, т.к.
+      // системно прописные и так тяжелее заглавных.
+      return b.localeCompare(a);
+    });
+  }
 }
