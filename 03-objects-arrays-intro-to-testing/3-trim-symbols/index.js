7912dbc087baaa3e6f53833736f45c6e1f9cf4fa
Denis Danshin <dosug.d@gmail.com>
2021-05-07 15:14:01 +0300

    функция ограниченного количества идущих подряд одинаковых символов

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!string.length || size === 0) return '';
+  if (!size) return string;
 
+  let newStr = '';
+
+  for (let i = 0, 
+           prevSymb = '', 
+           currSymb = '', 
+           sizeLeft = size; 
+           i < string.length; 
+           i++
+    ) {
+    currSymb = string[i];
+    prevSymb = string[i - 1] || '';
+
+    if (prevSymb !== currSymb) {
+      sizeLeft = size;
+    }
+
+    if (sizeLeft) {
+      newStr += currSymb;
+      sizeLeft--;
+    }
+  }
+
+  return newStr;
 }
