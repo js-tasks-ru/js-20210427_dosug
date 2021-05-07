5ae06362027f0841d8c1403949f25b5a8cbbe8e2
Denis Danshin <dosug.d@gmail.com>
2021-05-07 14:44:03 +0300

    функция инвертирования свойств и значений объекта

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) return;
 
+  const arEntries = Object.entries(obj);
+  if (!arEntries.length) return {};
+  
+  return Object.fromEntries(arEntries.map(item => {
+      return item.reverse();
+    })
+  )
 }
