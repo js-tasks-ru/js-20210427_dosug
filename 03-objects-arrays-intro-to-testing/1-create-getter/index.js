ce7091d3b681e40f27d6d3b4f4ddf45dd3578de9
Denis Danshin <dosug.d@gmail.com>
2021-05-07 14:26:09 +0300

    функция геттер

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arPath = path.split('.');
 
+  return function fn(obj, index = 0) {
+    if (obj === undefined) return;
+
+    if (index === arPath.length - 1) {
+      return obj[arPath[index]];
+    } else {
+      return fn(obj[arPath[index]], ++index);
+    }
+  }
 }
